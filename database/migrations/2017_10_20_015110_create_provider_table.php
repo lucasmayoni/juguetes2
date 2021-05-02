a82a1cd9c3a8b314581fe6c83d8377aeeae72aa1
Lucas Mayoni <lucasmayoni@gmail.com>
2021-05-02 16:43:42 -0300

    Fixes and permissions.

--- database/migrations/2017_10_20_015110_create_provider_table.php
+++ database/migrations/2017_10_20_015110_create_provider_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateProviderTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        //
+        Schema::create('provider', function (Blueprint $table) {
+
+          $table->increments('id');
+          $table->string('name');
+          $table->string('link');
+          $table->boolean('status');
+          $table->timestamps();
         });
     }
 
@@ -27,6 +31,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
+          Schema::dropIfExists('provider');
     }
 }
